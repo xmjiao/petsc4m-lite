06328b6ab20dc6bfcb954f7b7d77a6b0ed2fae82
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2022-11-05 09:09:12 -0400

    Added nojvm mode.

--- matlab/build_petsc4m.m
+++ matlab/build_petsc4m.m
@@ -4,16 +4,16 @@ curpath = pwd;
 cleanup = onCleanup(@()cd(curpath));
 cd(petsc4m_root);
 
-files = dir('sys/petscGet*.m');
+files = dir('sys/petsc*.m');
 for i = 1:length(files)
     base = files(i).name(1:end - 2);
     run(fullfile('sys', 'lib', base, ['mex_', base, '.m']));
 end
 
-if exist('OCTAVE_VERSION', 'builtin') || ~usejava('jvm') && ~isempty(getenv('LD_PRELOAD'))
+if exist('OCTAVE_VERSION', 'builtin') || ~usejava('jvm')
     run(fullfile('CRS/lib/petscSolveCRS/mex_petscSolveCRS.m'));
 else
-    % If running with MATLAB with JVM or without LD_PRELOAD, use exe files
+    % If running with MATLAB with JVM, use exe files
     run(fullfile('CRS/lib/petscSolveCRS/build_petscSolveCRS_exe.m'));
 end
 
