88188bf08d4509ff1b943a11ac8bca21ad0ac4da
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2022-11-06 18:23:33 -0500

    use PETSC_PRELOAD

--- matlab/utils/init_petsc.m
+++ matlab/utils/init_petsc.m
@@ -5,13 +5,6 @@ if exist(['petscInitialize.' mexext], 'file') && ...
     isequal(which('petscInitialize'), which(['petscInitialize.' mexext]))
     try
         if ~petscInitialized
-            if ~exist('OCTAVE_VERSION', 'builtin') && usejava('jvm')
-                warning('Petsc4m:JavaConflict', ...
-                    ['Petsc4m is prone to crashing in MATLAB desktop environment ', ...
-                    newline, 'due to apparent conflict with JAVA. Consider starting MATLAB', ...
-                    newline, 'in command-line mode without JAVA with command `matlab -nojvm`', ...
-                    newline, 'when using Petsc4m.']);
-            end
             petscInitialize;
 
             if exist('OCTAVE_VERSION', 'builtin')
@@ -21,7 +14,7 @@ if exist(['petscInitialize.' mexext], 'file') && ...
     catch
         warning('Petsc4m:FailedInit', 'Failed to initialize petsc4m.')
         if exist('OCTAVE_VERSION', 'builtin')
-            warning('Try to set LD_LIBRARY_PATH=$PETSC_DIR/bin in shell and restart Octave');
+            warning('Try to set LD_LIBRARY_PATH=$PETSC_DIR/lib in shell and restart Octave');
         end
     end
 else
